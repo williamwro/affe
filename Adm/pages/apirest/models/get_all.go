bc76ce83577682989180918c9ebcb537f60ee583
williamwro <william@makecard.com.br>
2023-07-06 10:31:47 -0300

    goland

--- Adm/pages/apirest/models/get_all.go
+++ Adm/pages/apirest/models/get_all.go
@@ -1,40 +1,27 @@
 package models
 
 import (
-	"log"
+	"context"
 
 	"makecard.com.br/db"
+
+	dbsql "makecard.com.br/dbsql/db"
 )
 
-func GetAll() (empregadores []Empregador, err error) {
+func GetAll() (empregadores []dbsql.Empregador, err error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
 		return
 	}
 	defer conn.Close()
 
-	err = conn.Ping()
-	if err != nil {
-		log.Printf("Erro ao conectar o banco de dados", err)
-	}
-
 	conn.Exec(`set search_path='sind'`)
 
-	rows, err := conn.Query(`SELECT id, nome, responsavel, telefone, abreviacao, divisao FROM empregador;`)
-	if err != nil {
-		return
-	}
+	dt := dbsql.New(conn)
 
-	for rows.Next() {
-		var empregador Empregador
+	ctx := context.Background()
 
-		err = rows.Scan(&empregador.ID, &empregador.nome, &empregador.responsavel, &empregador.telefone, &empregador.abreviacao, &empregador.divisao)
-		if err != nil {
-			continue
-		}
-
-		empregadores = append(empregadores, empregador)
-	}
+	empregadores, err := dt.ListEmpregadores(ctx)
 
 	return
 }
