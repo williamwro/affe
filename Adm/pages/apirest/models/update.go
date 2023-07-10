90455e5ab8ec7c8c482a3c562363158eaf4a9fa5
William R Oliveira <william@makecard.com.br>
2023-07-10 19:19:45 -0300

    goland

--- Adm/pages/apirest/models/update.go
+++ Adm/pages/apirest/models/update.go
@@ -1,21 +1,37 @@
 package models
 
-import "makecard.com.br/db"
+import (
+	"context"
+	"log"
 
-func Update(id int64, empregador Empregador) (int64, error) {
+	"makecard.com.br/db"
+	dbsql "makecard.com.br/dbsql/db"
+)
+
+func Update(id int64, empregador dbsql.UpdateEmpregadorParams) (int64, error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
 		return 0, err
 	}
 	defer conn.Close()
 
-	res, err := conn.Exec(`UPDATE sind.empregador
-	SET nome=$2, responsavel=$3, telefone=$4, abreviacao=$5, divisao=$6
-	WHERE id=$1`, id, empregador.nome, empregador.responsavel, empregador.telefone, empregador.abreviacao, empregador.divisao)
+	conn.Exec(`set search_path='sind'`)
+
+	dt := dbsql.New(conn)
+
+	ctx := context.Background()
+
+	empregador_result, err := dt.UpdateEmpregador(ctx, dbsql.UpdateEmpregadorParams(empregador))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(empregador_result)
+
 	if err != nil {
 		return 0, err
 	}
 
-	return res.RowsAffected()
+	return 0, err
 
 }
