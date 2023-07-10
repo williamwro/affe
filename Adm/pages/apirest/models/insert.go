f96a3f027a253d330d76e5f01fd469378c1489ea
William R Oliveira <william@makecard.com.br>
2023-07-10 19:19:34 -0300

    goland

--- Adm/pages/apirest/models/insert.go
+++ Adm/pages/apirest/models/insert.go
@@ -1,19 +1,32 @@
 package models
 
-import "makecard.com.br/db"
+import (
+	"context"
+	"log"
 
-func Insert(empregador Empregador) (id int64, err error) {
+	"makecard.com.br/db"
+	dbsql "makecard.com.br/dbsql/db"
+)
+
+func Insert(empregador dbsql.Empregador) (id int64, err error) {
 	conn, err := db.OpenConnection()
 	if err != nil {
 		return
 	}
 	defer conn.Close()
 
-	sql := `INSERT INTO sind.empregador(
-		    nome, responsavel, telefone, abreviacao, divisao)
-		    VALUES($1, $2, $3, $4, $5) RETURNING id`
+	conn.Exec(`set search_path='sind'`)
 
-	err = conn.QueryRow(sql, empregador.nome, empregador.responsavel, empregador.telefone, empregador.abreviacao, empregador.divisao).Scan(&id)
+	dt := dbsql.New(conn)
+
+	ctx := context.Background()
+
+	empregador_result, err := dt.CreateEmpregador(ctx, dbsql.CreateEmpregadorParams(empregador))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(empregador_result)
 
 	return
 }
