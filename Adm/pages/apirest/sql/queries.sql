cf56a631d90ae55f601b763f132a873944e72c07
William R Oliveira <william@makecard.com.br>
2023-07-10 19:19:54 -0300

    goland

--- Adm/pages/apirest/sql/queries.sql
+++ Adm/pages/apirest/sql/queries.sql
@@ -9,10 +9,10 @@ INSERT INTO empregador(
 	id, nome, responsavel, telefone, abreviacao, divisao)
 	VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;
 
--- name: UpdateEmpregador :exec
+-- name: UpdateEmpregador :one
 UPDATE empregador
 	SET id=$2, nome=$3, responsavel=$4, telefone=$5, abreviacao=$6, divisao=$7
-	WHERE Id=$1 RETURNING *;
+	WHERE id=$1 RETURNING *;
 
 -- name: DeleteEmpregador :exec
 DELETE FROM empregador
